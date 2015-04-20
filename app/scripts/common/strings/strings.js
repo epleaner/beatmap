a412479d1a2ad0fe5c673859079f03e35969e360
Eli Pleaner <epleaner@calpoly.edu>
2015-04-20 13:40:06 -0700

    Updated search string

--- app/scripts/common/strings/strings.js
+++ app/scripts/common/strings/strings.js
@@ -2,7 +2,7 @@ define(function(){
 	'use strict';
 
 	var strings = {
-		searchInfo: 'Find recommendations based on artists and albums.',
+		searchInfo: 'Find recommendations based on artists and albums, and listen anywhere.',
 		searchAlbumInstructions: 'To search with an album, use "album - artist".',
 		searchPlaceholder: 'Enter an artist or album'
 	};
