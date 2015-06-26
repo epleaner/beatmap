abe05ecdf4d67013ff2f23c689e2821bce573178
Eli Pleaner <epleaner@calpoly.edu>
2015-06-25 21:17:47 -0700

    Some work on dialogs

--- Gruntfile.js
+++ Gruntfile.js
@@ -236,7 +236,7 @@ module.exports = function(grunt) {
                     cwd: '<%= yeoman.app %>',
                     dest: '<%= yeoman.dist %>',
                     src: [
-                        '*.{ico,txt}',
+                        '*.{ico,png,txt}',
                         '.htaccess',
                         'images/**/*.{webp,gif,svg,png}',
                         '*.html',
